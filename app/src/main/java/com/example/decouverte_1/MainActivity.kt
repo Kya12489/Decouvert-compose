5aa2ee2057adf99f5a3c45c8186ed86ed22d9845
lrichard <lawric49@gmail.com>
2025-11-27 08:42:05 +0100

    je sais plus

--- app/src/main/java/com/example/decouverte_1/MainActivity.kt
+++ app/src/main/java/com/example/decouverte_1/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.decouverte_1
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.decouverte_1.ui.theme.Decouverte1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +33,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Decouverte1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MyApp(
                         modifier = Modifier.padding(innerPadding)
+                        , names = listOf("World","Lawrence")
                     )
                 }
             }
@@ -31,17 +44,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 50.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello TOI!",
-        modifier = modifier
-    )
+    val padding = remember { mutableStateOf(Modifier.padding(24.dp)) }
+    val expended = remember { mutableStateOf(false) }
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+
+        Row(modifier = padding.value) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            Button(onClick = {
+                expended.value = !expended.value
+                if (expended.value)
+                    padding.value = Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp,bottom = 500.dp)
+                else
+                    padding.value = Modifier.padding(all = 24.dp)
+
+            }){
+                Text(text = if (expended.value) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 500)
 @Composable
 fun GreetingPreview() {
     Decouverte1Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
